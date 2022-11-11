a63e2959a862e1f6c14494e0ae903dd54ef3c7b1
lylyzyta <lylyzyta@gmail.com>
2022-11-11 14:12:34 -0600

    Detalle de Tarjeta

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useEffect, useState } from "react";
 import getProducts from "../helpers/api";
 import styles from "./ProductList.module.css";
+import Popup from "./Popup";
+
 
 function ProductList() {
   const [products, setProducts] = useState([]);
@@ -10,11 +12,20 @@ function ProductList() {
       setProducts(json)
     );
   }, []);
+
+  const [isOpen, setIsOpen] = useState(false);
+ const [uniqueProduct, setUniqueProduct] = useState([]);
+  const togglePopup = (productID) => {
+    setIsOpen(!isOpen);
+    fetch(`https://gnk.onm.mybluehost.me/products_api/${productID}`)
+    .then((response) => response.json())
+    .then((data) => setUniqueProduct(data));
+  };
  
   return (
     <section className={styles.productsContainer}>
                  {products.map((product) => (
-            <section  className={styles.cardStyle} key={product.id} >
+            <section  className={styles.cardStyle} key={product.id}  >
              <>
              <img className={styles.productsImg} src={product.img}
                alt="logo-icon"/>
@@ -23,6 +34,38 @@ function ProductList() {
             <span  className={styles.productsTitle}>{product.title}</span>
             <span className={styles.productsPrice}>Precio : $ {product.price}</span> 
            </>
+
+           <button
+                    className={styles.productAdd}
+                    onClick={() => togglePopup(product.id)}
+                  >
+                    {" "}
+                    Description{" "}
+                  </button>
+
+           {isOpen && (
+                    <Popup
+                      content={
+                        <>
+                          <b className={styles.productDetail}>
+                          {uniqueProduct}
+                          
+                          </b>
+                          <br></br>
+                          <br></br>
+                          <br></br>
+
+                          <button
+                            className={styles.productClose}
+                            onClick={togglePopup}
+                          >
+                            Close
+                          </button>
+                        </>
+                      }
+                      handleClose={togglePopup}
+                    />
+                  )}
            </section>
          ))}
 
